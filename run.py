41e6f8fdcd3290e81fb83c61cf34d6677797f928
r3b3l-dev <wayne.manning@tuta.io>
2022-03-11 15:21:40 +0000

    Creat seimple flas akpp to return the text Hello World

---
+++ run.py
@@ -0,0 +1,26 @@
+# import os from the standard library
+import os
+# import the flask class
+from flask import Flask
+
+# create an instance of flask and store it in the varibale called app
+# the first argument of the flask class is the name of the applications module/package
+# since we're just using a single module we're just using __name__ which is a built in 
+# python variable. Flask needs this so it knows where to look for templates and static files 
+app = Flask(__name__)
+
+# use the route decorator to tell flask what url should trigger the funtion that follows
+# a decorator starts with the @ symbol. A way of wrapping funtions.
+# when we navigate to / root index() is triggered.
+@app.route("/")
+def index():
+    return "Hello, World"
+
+# if name is = main then we're going to run our app with 
+# the following arguments
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        # allows us to debug code in dev stage
+        debug=True)
